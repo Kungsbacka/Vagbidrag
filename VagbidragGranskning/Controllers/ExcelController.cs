ac5609033e63bcb743960e4e829332d5945d4c26
marott <marott@ADMDSEM031601>
2017-02-02 16:04:53 +0100

    La till lite excelfunktionalitet

---
+++ VagbidragGranskning/Controllers/ExcelController.cs
@@ -0,0 +1,97 @@
+﻿using KBA.TE.Models;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.IO;
+using KBA.TE.Models;
+
+namespace VagbidragGranskning.Controllers
+{
+    public class ExcelController : ApiController
+    {
+        // GET: api/Excel
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Excel/5
+        public HttpResponseMessage Get(int id)
+        {     
+
+            return createExcel();
+        }
+
+        // POST: api/Excel
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Excel/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Excel/5
+        public void Delete(int id)
+        {
+        }
+
+        private void TestDb()
+        {
+            //Använd VFSQLPopulator
+            VFSQLPopulator db = new VFSQLPopulator();
+
+
+        }
+
+        private HttpResponseMessage createExcel()
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            MemoryStream ms = new MemoryStream();
+            response.Content = new StreamContent(ms);
+            //Skapa ett tillfälligt excel
+            using (ExcelPackage doc = new ExcelPackage(ms))
+            {
+                //Create the worksheet
+                ExcelWorksheet workSheet = doc.Workbook.Worksheets.Add("Vagbidrag");
+                
+                //Headers
+                /*workSheet.Cells["A1"].Value = "nr";
+                workSheet.Cells["B1"].Value = "vaghallare";
+                workSheet.Cells["C1"].Value = "bank_post";
+                workSheet.Cells["D1"].Value = "orgnr";
+                workSheet.Cells["E1"].Value = "total_vaglangd";
+                workSheet.Cells["F1"].Value = "statligt_bidrag";
+                workSheet.Cells["G1"].Value = "statligt_bidragsgrundande";
+                workSheet.Cells["H1"].Value = "vaglangd_gc";
+                workSheet.Cells["A1:H1"].Style.Font.Bold = true;*/
+
+                VagbidragSQLPopulator bidragPop = new VagbidragSQLPopulator();
+                //Load the datatable into the sheet, starting from cell A1.
+                workSheet.Cells["A1"].LoadFromDataTable(bidragPop.getRapportTable(), true);
+                
+                //Format
+                workSheet.Cells.AutoFitColumns();
+                workSheet.Row(1).Style.Font.Bold = true;
+
+                string filePath = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                response.Content.Headers.ContentType =  new System.Net.Http.Headers.MediaTypeHeaderValue(MimeMapping.GetMimeMapping(filePath));
+                response.Content.Headers.ContentDisposition =  new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                response.Content.Headers.ContentDisposition.FileName = "Vagbidrag.xlsx";
+
+                doc.Save();
+                ms.Seek(0,0);
+                   
+                
+            }
+            return response;
+        }
+    }
+}
---
+++ VagbidragGranskning/Models/VagbidragSQLPopulator.cs
@@ -0,0 +1,65 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace KBA.TE.Models
+{
+    public class VagbidragSQLPopulator
+    {
+
+        public IEnumerable<Vagforening> list()
+        {
+            List<Vagforening> result = new List<Vagforening>();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection())
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["kbakarta"].ConnectionString;
+                connection.Open();
+                command.Connection = connection;
+                DataTable resultTable = new DataTable();
+                command.CommandText = "SELECT * FROM vagforeningar_rapport";
+                resultTable.Load(command.ExecuteReader());
+                connection.Close();
+                foreach (DataRow row in resultTable.Rows)
+                {
+                    Vagforening vf = load(row);
+                    result.Add(vf);
+                }
+            }
+            return result;
+        }
+        public DataTable getRapportTable()
+        {
+            DataTable result = new DataTable();
+
+            using (NpgsqlConnection connection = new NpgsqlConnection())
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["kbakarta"].ConnectionString;
+                connection.Open();
+                command.Connection = connection;
+                //command.CommandText = "SELECT * FROM vagforeningar_rapport";
+                command.CommandText = "SELECT nr, vaghallare, bank_post, orgnr, total_vaglangd, statlig_vaglangd, statligt_bidrag, statligt_bidragsgrundande, vaglangd_gc FROM vagforeningar_rapport";
+                result.Load(command.ExecuteReader());
+                connection.Close();
+               
+            }
+            return result;
+        }
+        private Vagforening load(DataRow row)
+        {
+            Vagforening result = new Vagforening();
+
+            
+            result.nr = row["nr"]?.ToString();
+            
+
+            return result;
+        }
+    }
+}
